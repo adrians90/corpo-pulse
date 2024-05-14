c151dc0b88fc752fcad720afa8ebf038f69b52db
adrians90 <adriansandu9090@gmail.com>
2024-05-14 16:28:22 +0100

    Make feedbackform a controlled element.

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const charCount = 150 - text.length;
   return (
     <form className="form">
       <textarea
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
         id="feedback-textarea"
         placeholder="bla"
         spellCheck={false}
@@ -11,7 +19,7 @@ export default function FeedbackForm() {
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
