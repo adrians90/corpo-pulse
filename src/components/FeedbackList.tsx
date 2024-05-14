f8e4f3f5cfd2b87c607090cc60fab5585e535af4
adrians90 <adriansandu9090@gmail.com>
2024-05-14 16:45:32 +0100

    Extract FeedbackItem in separate component

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,26 +1,9 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import FeedbackItem from "./FeedbackItem";
 
 export default function FeedbackList() {
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>593</span>
-        </button>
-        <div>
-          <p>B</p>
-        </div>
-        <div>
-          <p>ByteGrad</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
-            reprehenderit, voluptatem exercitationem consequuntur deleniti
-            blanditiis!
-          </p>
-        </div>
-        <p>4d</p>
-      </li>
+      <FeedbackItem />
     </ol>
   );
 }
