2e9c0a2940d276dfcebfd8bc23ec57ad100820e1
adrians90 <adriansandu9090@gmail.com>
2024-05-14 16:01:53 +0100

    Add form and textarea to FeedbackForm

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,3 +1,21 @@
 export default function FeedbackForm() {
-  return <div>FeedbackForm</div>;
+  return (
+    <form className="form">
+      <textarea
+        id="feedback-textarea"
+        placeholder="bla"
+        spellCheck={false}
+        maxLength={150}
+      />
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
+      <div>
+        <p className="u-italic">150</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
 }
