98a0a9ad1fd8d69a07487f0c65192340dd09cc7f
adrians90 <adriansandu9090@gmail.com>
2024-05-14 14:42:28 +0100

    Add css from external resource

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,3 +1,14 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <small>
+        <p>
+          &copy; Copyright{" "}
+          <a href="https://portfolio-adrians90.vercel.app/" target="_blank">
+            Adrian Sandu
+          </a>
+        </p>
+      </small>
+    </footer>
+  );
 }
