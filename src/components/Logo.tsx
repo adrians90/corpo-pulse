50deb8f5e64753b94330f92f2c0629f98ea91abc
adrians90 <adriansandu9090@gmail.com>
2024-05-16 15:39:16 +0100

    Change logo import syntax

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,9 @@
+import logo from "../assets/CorpoPulse.png";
+
 export default function Logo() {
   return (
     <a href="/" className="logo">
-      <img src="../../src/assets/CorpoPulse.png" alt="logo" />
+      <img src={logo} alt="logo" />
     </a>
   );
 }
