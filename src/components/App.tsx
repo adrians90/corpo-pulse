cbf4dc1ef4b4172f12511175f3156feddf7abb20
adrians90 <adriansandu9090@gmail.com>
2024-05-16 12:59:13 +0100

    Create context provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,9 +5,6 @@ import HashtagList from "./hashtag/HashtagList";
 import { TFeedbackItem } from "../lib/Types";
 
 function App() {
-  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
   const [selectedCompany, setSelectedCompany] = useState("");
 
   const filteredFeedbackItems = useMemo(
@@ -20,72 +17,10 @@ function App() {
     [feedbackItems, selectedCompany]
   );
 
-  const companyList = useMemo(
-    () =>
-      feedbackItems
-        .map((item) => item.company)
-        .filter((company, index, array) => {
-          return array.indexOf(company) === index;
-        }),
-    [feedbackItems]
-  );
-
-  const handleAddToList = async (text: string) => {
-    const companyName = text
-      .split(" ")
-      .find((word) => word.includes("#"))!
-      .substring(1);
-    const newItem: TFeedbackItem = {
-      id: new Date().getTime(),
-      text: text,
-      upvoteCount: 0,
-      daysAgo: 0,
-      company: companyName,
-      badgeLetter: companyName.substring(0, 1).toUpperCase(),
-    };
-
-    setFeedbackItems([...feedbackItems, newItem]);
-
-    await fetch(
-      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
-      {
-        method: "POST",
-        body: JSON.stringify(newItem),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }
-    );
-  };
-
   const handleSelectCompany = (company: string) => {
     setSelectedCompany(company);
   };
 
-  useEffect(() => {
-    const fetchFeedbackItems = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await fetch(
-          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
-        );
-
-        if (!response.ok) {
-          throw new Error();
-        }
-        const data = await response.json();
-        setFeedbackItems(data.feedbacks);
-      } catch (error) {
-        setErrorMessage("Something went wrong");
-      }
-
-      setIsLoading(false);
-    };
-
-    fetchFeedbackItems();
-  }, []);
   return (
     <div className="app">
       <Footer />
---
+++ src/components/contexts/FeedbackItemsContextProvider.tsx
@@ -0,0 +1,92 @@
+import { createContext, useEffect, useMemo, useState } from "react";
+import { TFeedbackItem } from "../../lib/Types";
+
+type TFeedbackItemsContext = {
+  feedbackItems: TFeedbackItem[];
+  isLoading: boolean;
+  errorMessage: string;
+  companyList: string[];
+  handleAddToList: (text: string) => void;
+};
+
+const FeedbackItemsContext = createContext<TFeedbackItemsContext | null>(null);
+
+export default function FeedbackItemsContextProvider() {
+  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const companyList = useMemo(
+    () =>
+      feedbackItems
+        .map((item) => item.company)
+        .filter((company, index, array) => {
+          return array.indexOf(company) === index;
+        }),
+    [feedbackItems]
+  );
+
+  const handleAddToList = async (text: string) => {
+    const companyName = text
+      .split(" ")
+      .find((word) => word.includes("#"))!
+      .substring(1);
+    const newItem: TFeedbackItem = {
+      id: new Date().getTime(),
+      text: text,
+      upvoteCount: 0,
+      daysAgo: 0,
+      company: companyName,
+      badgeLetter: companyName.substring(0, 1).toUpperCase(),
+    };
+
+    setFeedbackItems([...feedbackItems, newItem]);
+
+    await fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+      {
+        method: "POST",
+        body: JSON.stringify(newItem),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage("Something went wrong");
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+  }, []);
+  return (
+    <FeedbackItemsContext.Provider
+      value={{
+        feedbackItems,
+        isLoading,
+        errorMessage,
+        companyList,
+        handleAddToList,
+      }}
+    ></FeedbackItemsContext.Provider>
+  );
+}
